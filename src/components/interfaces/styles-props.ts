f201bddc5e7fec28a48ff221a031159811e09338
Edgar <konst-awe@mail.ru>
2022-11-30 20:14:29 +0400

    add styles types

---
+++ src/components/interfaces/styles-props.ts
@@ -0,0 +1,15 @@
+export type TSize = "small" | "normal" | "large";
+
+export type TFontWeight =
+  | "100"
+  | "200"
+  | "300"
+  | "400"
+  | "500"
+  | "600"
+  | "700"
+  | "800"
+  | "900";
+
+export type THeight = "full";
+//TODO: add font family, fontSize, fontColor
